0790bd81074101c0adf32b845903826948bf1ccd
Yeehaa <medialoperations@gmail.com>
2012-08-09 14:11:12 +0200

    add rspec test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,101 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	before do 
+		@user = User.new(name: "Example User", email: "user@example.com",
+						 password: 'foobar', password_confirmation: 'foobar')
+	end
+
+	subject { @user }
+
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
+	it { should respond_to(:encrypted_password) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:valid_password?) }
+
+
+	it { should be_valid }
+	
+	describe "when name is not present" do
+		before { @user.name = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when name is too long" do
+		before { @user.name = "a" * 51 }
+		it { should_not be_valid}
+	end
+
+	describe "when email is not present" do
+		before { @user.email = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when email format is invalid" do
+		it "should be invalid" do
+			addresses = %w[user@foo,com user_at_foo.org example.user@foo. 
+										 foo@bar_baz.com foo@bar+baz.com]
+			addresses.each do | invalid_address |
+				@user.email = invalid_address
+				@user.should_not be_valid
+			end
+		end
+	end
+
+	describe "when email format is valid" do
+		it "should be valid" do
+			addresses = %w[user@foo.COM A_US-ER@f.b.org frst.1st@foo.jp a+b@baz.cn]
+			addresses.each do | valid_address |
+				@user.email = valid_address
+				@user.should be_valid
+			end
+		end
+	end
+
+	describe "when email address is already taken" do
+		before do
+			user_with_same_email = User.new(name: "Alt User", email: "user@example.com",
+			 															  password: 'foobar', password_confirmation: 'foobar')
+			user_with_same_email.email = @user.email.upcase
+			user_with_same_email.save
+		end
+
+		it { should_not be_valid }
+	end
+
+	describe "when password is not present" do
+		before { @user.password = @user.password_confirmation = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when password does not match confirmation" do
+		before { @user.password_confirmation = "foobaz" }
+		it { should_not be_valid }
+	end
+
+	describe "when password confirmation is nil" do
+		before { @user.password_confirmation = nil }
+		it { should_not be_valid }
+	end
+
+	describe "return valid_password?" do
+		before { @user.save }
+		let(:found_user) { User.find_by(email: @user.email) }
+
+		describe "with valid password" do
+			it { found_user.valid_password?(@user.password) == true	}
+		end
+
+		describe "with invalid password" do
+			it { found_user.valid_password?("bla") == true	}
+		end
+	end
+
+	describe "with a password that is too short" do
+		before { @user.password = @user.password_confirmation = "a" * 5 }
+		it { should be_invalid }	
+	end
 end
