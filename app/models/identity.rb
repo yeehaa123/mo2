a2df5c968fcb7dc3217f58bae0784f8da895704f
Yeehaa <medialoperations@gmail.com>
2012-08-17 12:44:02 +0200

    add validations on user and identity

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -5,4 +5,8 @@ class Identity
   field :name, type: String
   field :email, type: String
   field :password_digest, type: String
+
+  validates :name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
 
 describe Identity do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	let(:identity) { FactoryGirl.create(:identity) }
+	
+	subject { identity }
+	
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
+
+	it { should be_valid }
+
+	describe "when name is not present" do
+		before { identity.name = " " }
+		it { should_not be_valid}
+	end
+
+	describe "when name is too long" do
+		before { identity.name = "a" * 51 }
+		it { should_not be_valid}
+	end
+	
+	describe "when email is not present" do
+		before { identity.email = " " }
+		it { should_not be_valid}
+	end
+
+	describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+      	identity.email = invalid_address
+        identity.should_not be_valid
+      end      
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        identity.email = valid_address
+        identity.should be_valid
+      end      
+    end
+  end
 end
